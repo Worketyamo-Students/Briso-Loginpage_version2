8583237156ebc949d27dc8bf01b8c2b76815f5ec
Briso10 <Kouambrice10@gmail.com>
2024-04-19 03:07:44 +0100

    version_mobile

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Projet : Login page
+
+## Description
+Ceci est une version ameliorer de la login page faite precedement avec une net amelioration dans la syntax et nouvelle notions introduite
+
+## Langages utilises
+1.HTML
+2.Tailwind CSS
---
+++ assets/tailwind.config.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        // Variabilisation des couleur
+        colors: {
+          primary: '#7286D3',
+          secondary: '#8EA7E9',
+          tcolor:  '#19192C', 
+        }
+      }
+    }
+  }
